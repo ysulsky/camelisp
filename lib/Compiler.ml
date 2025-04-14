05bca530a8d79ed7f6610f048e1cbfb9002501a7
Yury Sulsky <yury.sulsky@gmail.com>
2025-04-14 04:18:01 -0700

    Switch to native compilation.

--- lib/Compiler.ml
+++ lib/Compiler.ml
@@ -9,11 +9,11 @@ module Value = Value
 (* --- Compiler Paths --- *)
 (* Use ocamlfind to handle package paths *)
 let ocamlfind_path = "ocamlfind"
-(* We still need the base compiler name for ocamlfind *)
-let ocamlc_path = "ocamlc"
+(* Switch to ocamlopt for native code generation *)
+let ocamlopt_path = "ocamlopt"
 (* Keep include paths for project's own modules *)
 let include_paths = ["_build/default/lib/"]
-(* Library paths for linking against scaml.cma itself *)
+(* Library paths might still be needed for ocamlfind *)
 let library_paths = ["_build/default/lib/"]
 
 
@@ -28,14 +28,16 @@ let last_loaded_environment : (string * Value.t) list option ref = ref None
 (* --- End Shared State --- *)
 
 
-(** Compiles an OCaml source string to a .cma file and loads it *)
-(* Modified: Returns the environment list directly, retrieved from the shared ref *)
+(** Compiles an OCaml source string to a .cmxs file and loads it *)
+(* Modified: Compiles to native code (.cmxs) and returns env list *)
 let compile_and_load_string (ocaml_code : string) : (string * Value.t) list =
   let ml_filename = Filename_unix.temp_file "scaml_generated_" ".ml" in
   let base_filename = Filename.chop_extension ml_filename in
-  let cmo_filename = base_filename ^ ".cmo" in
-  let cma_filename = base_filename ^ ".cma" in
-  let cmi_filename = base_filename ^ ".cmi" in (* Define cmi for cleanup *)
+  (* Native compilation outputs *)
+  let cmx_filename = base_filename ^ ".cmx" in
+  let o_filename = base_filename ^ ".o" in
+  let cmxs_filename = base_filename ^ ".cmxs" in (* Native shared library *)
+  let cmi_filename = base_filename ^ ".cmi" in
 
 
   (* Reset shared ref before loading the new module *)
@@ -45,33 +47,37 @@ let compile_and_load_string (ocaml_code : string) : (string * Value.t) list =
     (* 1. Write the generated OCaml code to the temp .ml file *)
     Out_channel.write_all ml_filename ~data:ocaml_code;
 
-    (* 2. Compile the .ml to .cmo (bytecode object file) *)
+    (* 2. Compile the .ml to .cmx/.o (native object file) *)
     let include_flags = List.map include_paths ~f:(sprintf "-I %s") |> String.concat ~sep:" " in
-    (* Make sure scaml package is included for Value, Runtime etc. *)
-    let compile_byte_cmd =
-      sprintf "%s %s -package core,scaml -linkpkg -c %s -o %s %s"
-        ocamlfind_path ocamlc_path include_flags cmo_filename ml_filename
+    (* Use ocamlopt -c. Ensure scaml package is included for interfaces *)
+    let compile_native_cmd =
+      sprintf "%s %s -package core,scaml -linkpkg -c %s -o %s" (* Output handled by -o *)
+        ocamlfind_path ocamlopt_path include_flags ml_filename
+        (* Note: -o file.cmx implicitly creates file.o and file.cmi *)
     in
-    if Sys_unix.command compile_byte_cmd <> 0 then
-      raise (Compilation_error (sprintf "OCaml compilation failed for %s. Command: %s" ml_filename compile_byte_cmd));
+    (* Printf.printf "Executing: %s\n%!" compile_native_cmd; *)
+    if Sys_unix.command compile_native_cmd <> 0 then
+      raise (Compilation_error (sprintf "OCaml native compilation failed for %s. Command: %s" ml_filename compile_native_cmd));
 
-    (* 3. Link the .cmo into a .cma (bytecode library archive) *)
+    (* 3. Link the .cmx/.o into a .cmxs (native shared library) *)
     let lib_flags = List.map library_paths ~f:(sprintf "-I %s") |> String.concat ~sep:" " in
-    (* Ensure scaml.cma is linked *)
-    let link_byte_cmd =
-      sprintf "%s %s -package core,scaml -linkpkg -a %s %s -o %s %s" (* Added scaml package *)
-        ocamlfind_path ocamlc_path lib_flags "scaml.cma" cma_filename cmo_filename
+    (* Use ocamlopt -shared. Link against scaml package *)
+    let link_native_cmd =
+      sprintf "%s %s -package core,scaml -linkpkg -shared %s -o %s %s"
+        ocamlfind_path ocamlopt_path lib_flags cmxs_filename cmx_filename
+        (* We provide the .cmx, ocamlopt finds the corresponding .o *)
     in
-    if Sys_unix.command link_byte_cmd <> 0 then
-       raise (Compilation_error (sprintf "OCaml linking failed for %s. Command: %s" ml_filename link_byte_cmd));
+    (* Printf.printf "Executing: %s\n%!" link_native_cmd; *)
+    if Sys_unix.command link_native_cmd <> 0 then
+       raise (Compilation_error (sprintf "OCaml native linking failed for %s. Command: %s" ml_filename link_native_cmd));
 
-    (* 4. Dynamically load the .cma library *)
-    (* Dynlink.loadfile executes the top-level code of the loaded module, *)
-    (* which should now include the mutation of Compiler.last_loaded_environment *)
+    (* 4. Dynamically load the .cmxs native shared library *)
+    (* Dynlink.loadfile works for .cmxs in native executables *)
+    (* Printf.printf "Dynlinking: %s\n%!" cmxs_filename; *)
     begin try
-      Dynlink.loadfile cma_filename
+      Dynlink.loadfile cmxs_filename
     with Dynlink.Error err ->
-        raise (Compilation_error (sprintf "Dynlink error loading %s: %s" cma_filename (Dynlink.error_message err)))
+        raise (Compilation_error (sprintf "Dynlink error loading %s: %s" cmxs_filename (Dynlink.error_message err)))
     end;
 
     (* 5. Retrieve the environment from the shared ref *)
@@ -83,10 +89,12 @@ let compile_and_load_string (ocaml_code : string) : (string * Value.t) list =
 
   ) ~finally:(fun () ->
     (* Clean up temporary files *)
+    (* Printf.printf "Cleaning up temp files for %s\n%!" base_filename; *)
     Exn.handle_uncaught ~exit:false (fun () -> Sys_unix.remove ml_filename);
-    Exn.handle_uncaught ~exit:false (fun () -> Sys_unix.remove cmo_filename);
     Exn.handle_uncaught ~exit:false (fun () -> Sys_unix.remove cmi_filename);
-    Exn.handle_uncaught ~exit:false (fun () -> Sys_unix.remove cma_filename);
+    Exn.handle_uncaught ~exit:false (fun () -> Sys_unix.remove cmx_filename);
+    Exn.handle_uncaught ~exit:false (fun () -> Sys_unix.remove o_filename);
+    Exn.handle_uncaught ~exit:false (fun () -> Sys_unix.remove cmxs_filename);
     ()
   )
 
